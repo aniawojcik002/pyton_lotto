fedaba55f6fa744e7fc6444a5f0e49e8c47d78f2
ania.wojcik002 <anna.wojcik002@gmail.com>
2025-07-08 23:27:12 +0200

    Add random sample and fix variables

--- lotto_zero.py
+++ lotto_zero.py
@@ -1,29 +1,23 @@
-from random import randint
+from random import sample
 
-my_numbers = {1, 16, 49, 44, 2, 6}
+MY_NUMBERS = {1, 16, 49, 44, 2, 6}
+PRICE_LOTTO = 3
 
 def generate_lotto():
-    lotto_number = { randint(1,49), randint(1,49), randint(1,49), randint(1,49), randint(1,49), randint(1,49) }
-    while len(lotto_number) != 6:
-        lotto_number = generate_lotto()
+    lotto_number = set(sample(range(1,50), k=6))
     return lotto_number
-
 
 def count_lotto():
     lotto_numbers = generate_lotto()
 
-    price_lotto = 3
-    total_lotto = 0
-    counter_lotto = 0
-    while my_numbers != lotto_numbers:
-        total_lotto += price_lotto
+    counter_lotto = 1
+    while MY_NUMBERS != lotto_numbers:
         counter_lotto += 1
         lotto_numbers = generate_lotto()
 
 
-    print (f"Trzeba wydać {total_lotto} zł")
+    print (f"Trzeba wydać {counter_lotto * PRICE_LOTTO} zł")
     # 3 razy w tygodniu, 4 razy w miesiącu, 12 miesiecy
-    years = counter_lotto / 12 / 12
-    print (f"{years:.0f} - tyle lat trzeba grać")
+    print (f"{counter_lotto/144:.0f} - tyle lat trzeba grać")
 
 count_lotto()
